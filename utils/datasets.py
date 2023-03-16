a504c52d117bf89a618ade4704db24a0c28a98f1
jobWorkHub <93673633+jobWorkHub@users.noreply.github.com>
2023-03-15 19:14:21 -0700

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -387,7 +387,11 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
 
         # Check cache
         self.label_files = img2label_paths(self.img_files)  # labels
-        cache_path = (p if p.is_file() else Path(self.label_files[0]).parent).with_suffix('.cache')  # cached labels
+#         cache_path = (p if p.is_file() else Path(self.label_files[0]).parent).with_suffix('.cache')  # cached labels
+        if "Train" in self.label_files:
+            cache_path = '/kaggle/working/cache/train/labels.cache'
+        else:
+            cache_path = '/kaggle/working/cache/test/labels.cache'
         if cache_path.is_file():
             cache, exists = torch.load(cache_path), True  # load
             #if cache['hash'] != get_hash(self.label_files + self.img_files) or 'version' not in cache:  # changed
